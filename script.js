38e7ebf99b1037d0ab8651d4a3c5ae5f724c469d
Subaranjanibalasubramanian11 <subaranjanikb1102@gmail.com>
2026-03-11 17:18:06 +0530

    My portfolio website

---
+++ script.js
@@ -0,0 +1,73 @@
+function showResumeProject(){
+
+document.getElementById("projectDetails").innerHTML = `
+
+<h2>AI Resume Analyzer</h2>
+
+<p>
+
+Developed a Resume Analyzer web application that helps users evaluate and improve their resumes. The system allows users to upload resumes in PDF format, and it automatically scans and analyzes important sections such as education, technical skills, soft skills, projects, and achievements. Based on the information in the resume, the application identifies suitable job roles and checks whether the required skills are present. It also provides suggestions to improve the resume by highlighting missing skills, incomplete sections, and areas that need improvement. The application helps students and job seekers understand their strengths and improve their resumes according to industry expectations.
+</p>
+
+<h3>Tech Stack</h3>
+
+<span class="tech">Python</span>
+<span class="tech">Flask</span>
+<span class="tech">HTML</span>
+<span class="tech">CSS</span>
+
+<br><br>
+
+<a href="https://github.com/Subaranjanibalasubramanian11/resume-analyzer" target="_blank">View GitHub Project</a>
+
+<br><br>
+
+<img src="resume1.jpg" class="projectImage">
+<img src="resume2.jpg" class="projectImage">
+<img src="resume3.jpg" class="projectImage">
+<img src="resume4.jpg" class="projectImage">
+<img src="resume5.jpg" class="projectImage">
+
+`;
+
+}
+
+
+function showLibraryProject(){
+
+document.getElementById("projectDetails").innerHTML = `
+
+<h2>E Library Management System</h2>
+
+<p>
+
+Developed a web-based E-Library Management System that allows users to access and manage digital books easily. The system includes features such as user signup and login, QR code login, a dashboard to view library statistics, and options to upload and view books in the library. Users can browse available books, read uploaded PDFs, and get assistance through a built-in chatbot for library-related queries. The project focuses on making digital book management simple and user-friendly.
+</p>
+
+<h3>Tech Stack</h3>
+
+<span class="tech">Python</span>
+<span class="tech">Flask</span>
+<span class="tech">HTML</span>
+<span class="tech">CSS</span>
+
+<br><br>
+
+<a href="https://github.com/Subaranjanibalasubramanian11/E_library_management" target="_blank">View GitHub Project</a>
+
+<br><br>
+
+<img src="library1.jpg" class="projectImage">
+<img src="library2.jpg" class="projectImage">
+<img src="library3.jpg" class="projectImage">
+<img src="library4.jpg" class="projectImage">
+<img src="library5.jpg" class="projectImage">
+<img src="library6.jpg" class="projectImage">
+<img src="library7.jpg" class="projectImage">
+<img src="library8.jpg" class="projectImage">
+<img src="library9.jpg" class="projectImage">
+<img src="library10.jpg" class="projectImage">
+
+`;
+
+}
